f4610d6965600ca566cc005c9308d715cf104b33
chenshuooooo <90021327+chenshuooooo@users.noreply.github.com>
2021-11-23 12:34:55 +0000

    :heavy_plus_sign: 增加：数据可视化完善

--- pipelines.py
+++ pipelines.py
@@ -2,25 +2,28 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-import pymysql
-
-class Exp41Pipeline:
+import pymssql
+class Exp42Pipeline:
     def process_item(self, item, spider):
-         print(item['bTitle'])
-         print(item['bAuthor'])
-         print(item['bDate'])
-         print(item['bDate'])
-         print(item['bPrice'])
-         print(item['bDetail'])
-        #con = pymysql.connect(host='localhost',user='root',password='',db='dangdang',charset='utf8mb4')
-        #cursor=con.cursor()
-        #sql="insert into book(title,author,publish,dat,price,detail) valuse ('%s%s%s%s%s%s')"%(item['bTitle'],item['bAuthor'],item['bPublish'],item['bDate'],item['bPrice'],item['bDetail'])
-        #cursor.execute(sql)
-        #con.commit()
-        #cursor.close()
-        #con.close()
+         print("{:^10}\t{:^10}\t{:^10}\t{:^10}\t{:^10}\t{:^10}\t{:^10}\t".format
+      (item["Id"],item["Currency"],item["TSP"],
+      item["CSP"],item["TBP"],item["CBP"],item["Times"]))
+
+         connect = pymssql.connect(host='localhost', user='chenshuo', password='cs031904104',
+                                   database='cs031904104', charset='UTF-8')  # 连接到sql server数据库
+         cur = connect.cursor()  # 创建操作游标
+         #表的创建在数据库中完成
+         # 插入数据
+         try:
+             cur.execute(
+                 "insert into rate_cs (id,Currency,TSP,CSP,TBP,CBP,Times) values ('%d','%s','%s','%s','%s','%s','%s')" % (
+                     item['Id'], item['Currency'].replace("'", "''"), item['TSP'].replace("'", "''"),
+                     item['CSP'].replace("'", "''"), item['TBP'].replace("'", "''"),
+                     item['CBP'].replace("'", "''"), item['Times'].replace("'", "''")))
+             connect.commit()  # 提交命令
+         except Exception as er:
+             print(er)
+
+         connect.close()#关闭与数据库的连接
          return item
+
